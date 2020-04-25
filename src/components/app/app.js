7a1366bee397d1abe738b1dc41db9b2b33684e93
KonstantinKuksov <kkuksov@gmail.com>
2020-04-25 15:26:00 +0300

    feat: delete list items on trash button click

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,29 +1,44 @@
-import React from "react";
+import React, { Component } from "react";
 import AppHeader from "./../app-header";
 import ItemStatusFilter from "./../item-status-filter";
 import SearchPanel from "./../search-panel";
 import TodoList from "./../todo-list";
 import './app.css';
 
-const App = () => {
+export default class App extends Component {
 
-    const todoData = [
-        { label: 'Drink Coffee', important: false, id: 1 },
-        { label: 'Make Awesome App', important: true, id: 2 },
-        { label: 'Have a lunch', important: false, id: 3 }
-    ];
+    state = {
+        todoData: [
+            {label: 'Drink Coffee', important: false, id: 1},
+            {label: 'Make Awesome App', important: true, id: 2},
+            {label: 'Have a lunch', important: false, id: 3}
+        ]
+    };
 
-    return (
-        <div className="todo-app">
-            <AppHeader toDo={1} done={3} />
-            <div className="top-panel d-flex">
-                <SearchPanel />
-                <ItemStatusFilter />
+    deleteItem = (id) => {
+        this.setState( ( { todoData } ) => {
+            const newArray = todoData.filter( listItem => listItem.id !== id );
+            return {
+                todoData: newArray
+            }
+        } );
+    };
+
+    render() {
+
+        return (
+            <div className="todo-app">
+                <AppHeader toDo={1} done={3}/>
+                <div className="top-panel d-flex">
+                    <SearchPanel/>
+                    <ItemStatusFilter/>
+                </div>
+
+                <TodoList todos={this.state.todoData}
+                          onDeleted={(id) => {
+                              this.deleteItem(id)
+                          }}/>
             </div>
-
-            <TodoList todos={todoData} />
-        </div>
-    );
-};
-
-export default App;
+        );
+    }
+}
