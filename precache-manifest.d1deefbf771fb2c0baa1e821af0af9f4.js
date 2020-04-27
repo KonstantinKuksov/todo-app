64bf07335d6505842f19b032bcdea0a9747a31a8
KonstantinKuksov <kkuksov@gmail.com>
2020-04-27 15:43:00 +0300

    Updates

---
+++ precache-manifest.d1deefbf771fb2c0baa1e821af0af9f4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "215855f5ebf4e7c76e29b541a526fdf9",
+    "url": "/todo-app/index.html"
+  },
+  {
+    "revision": "685b40a11ec70df13aae",
+    "url": "/todo-app/static/css/main.fdc152bf.chunk.css"
+  },
+  {
+    "revision": "6fa9e65e0105fcaab918",
+    "url": "/todo-app/static/js/2.52f2e08d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todo-app/static/js/2.52f2e08d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "685b40a11ec70df13aae",
+    "url": "/todo-app/static/js/main.e050ff1a.chunk.js"
+  },
+  {
+    "revision": "a18b0427a0f95b3ac31a",
+    "url": "/todo-app/static/js/runtime-main.f880baca.js"
+  }
+]);
