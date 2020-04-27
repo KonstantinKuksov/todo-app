0791fe55238b3260672943479862080b9b0f1361
KonstantinKuksov <kkuksov@gmail.com>
2020-04-27 10:16:40 +0300

    feat: add input field to item-add-form, add onSubmit event

--- src/components/item-add-form/item-add-form.js
+++ src/components/item-add-form/item-add-form.js
@@ -2,17 +2,40 @@ import React, {Component} from "react";
 import './item-add-form.css';
 
 export default class ItemAddForm extends Component {
+
+    state = {
+        label: ''
+    };
+
+    onLabelChange = (event) => {
+      this.setState({
+          label: event.target.value
+      });
+    };
+
+    onSubmit = (event) => {
+        event.preventDefault();
+        this.props.addItem(this.state.label);
+        this.setState({
+            label: ''
+        });
+
+    };
+
     render() {
 
-       const { addItem } = this.props;
-
         return (
-            <div className={'item-add-form'}>
-                <button className={'btn btn-outline-secondary'}
-                        onClick={ () => addItem('Hello') }>
+            <form className={'item-add-form d-flex'}
+                  onSubmit={this.onSubmit}>
+                <input type={'text'}
+                       className={'form-control'}
+                       placeholder={'what needs to be done'}
+                       onChange={this.onLabelChange}
+                       value={this.state.label}/>
+                <button className={'btn btn-outline-secondary'} >
                     Add Item
                 </button>
-            </div>
+            </form>
         );
     }
 }
